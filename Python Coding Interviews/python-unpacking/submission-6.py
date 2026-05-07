ff79d1fe0d1474d0330277c9622cea292cc2919c
never-settle-4-less <64497600+never-settle-4-less@users.noreply.github.com>
2026-05-07 19:11:36 +0530

    Add: python-unpacking - submission-6

---
+++ Python Coding Interviews/python-unpacking/submission-6.py
@@ -0,0 +1,24 @@
+from typing import List, Tuple
+
+
+def sum_3_integers(triplet: List[int]) -> int:
+    x,y,z = triplet
+    print(triplet)
+    res = x + y + z
+    return res
+
+
+def compute_volume(box_dimensions: Tuple[int, int, int]) -> int:
+    x,y,z = box_dimensions
+    print(box_dimensions)
+    res = x * y * z
+    return res
+  
+
+# do not modify below this line
+print(sum_3_integers([1, 2, 3]))
+print(sum_3_integers([4, 6, 2]))
+
+print(compute_volume((1, 2, 3)))
+print(compute_volume((3, 2, 1)))
+print(compute_volume((3, 9, 7)))
