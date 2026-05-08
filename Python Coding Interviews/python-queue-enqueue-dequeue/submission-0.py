1f3f9fce3a81359ff0b5cc375bb4e82be52b073d
never-settle-4-less <64497600+never-settle-4-less@users.noreply.github.com>
2026-05-08 17:47:13 +0530

    Add: python-queue-enqueue-dequeue - submission-0

---
+++ Python Coding Interviews/python-queue-enqueue-dequeue/submission-0.py
@@ -0,0 +1,23 @@
+from typing import List, Deque
+from collections import deque
+
+
+def rotate_list(arr: List[int], k: int) -> Deque[int]:
+    if k == 0:
+        return arr
+
+    a = deque(arr)
+    for i in range(k):
+        a.append(a.popleft())
+    return a
+
+
+
+
+# do not modify below this line
+print(rotate_list([1, 2, 3, 4, 5], 0))
+print(rotate_list([1, 2, 3, 4, 5], 1))
+print(rotate_list([1, 2, 3, 4, 5], 2))
+print(rotate_list([1, 2, 3, 4, 5], 3))
+print(rotate_list([1, 2, 3, 4, 5], 4))
+print(rotate_list([1, 2, 3, 4, 5], 5))
