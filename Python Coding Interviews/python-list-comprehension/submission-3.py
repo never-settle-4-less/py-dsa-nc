b8062fe07ebc52a6431a0d4ec3c8c7286d4babaf
never-settle-4-less <64497600+never-settle-4-less@users.noreply.github.com>
2026-05-08 17:07:39 +0530

    Add: python-list-comprehension - submission-3

---
+++ Python Coding Interviews/python-list-comprehension/submission-3.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+def create_list_of_odds(n: int) -> List[int]:
+    my_list = [i for i in range(1, n + 1)]
+    #result = [i for i in my_list if i % 2 != 0]
+
+    result = []
+    for i in range(len(my_list)):
+        if(my_list[i] % 2 != 0):
+            result.append(my_list[i])
+        
+    return result
+
+
+
+
+# do not modify below this line
+print(create_list_of_odds(1))
+print(create_list_of_odds(5))
+print(create_list_of_odds(6))
+print(create_list_of_odds(10))
