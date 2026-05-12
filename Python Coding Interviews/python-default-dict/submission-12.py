612387aa809360833017e9046ef30232497b346e
never-settle-4-less <64497600+never-settle-4-less@users.noreply.github.com>
2026-05-12 11:43:35 +0530

    Add: python-default-dict - submission-12

---
+++ Python Coding Interviews/python-default-dict/submission-12.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+from typing import List, Dict
+
+
+def count_chars(s: str) -> Dict[str, int]:
+    freq = defaultdict(int)
+
+    for token in s:
+        freq[token] = freq[token] + 1
+    return freq
+
+
+def nested_list_to_dict(nums: List[List[int]]) -> Dict[int, List[int]]:
+    
+    d = defaultdict(list)
+
+    for sublist in nums:
+
+        key = sublist[0]
+        values = sublist[1:]
+
+        d[key].append(values)
+
+       
+    return d
+
+
+# do not modify below this line
+print(count_chars("hello"))
+print(count_chars("helloworld"))
+print(count_chars("areallylongstringwhyareyoureadingthishahalol"))
+
+print(nested_list_to_dict([[1, 2, 3], [4, 5, 6], [1, 4]]))
+print(nested_list_to_dict([[1, 2, 3, 4], [4, 5, 6, 7], [1, 4, 5, 6]]))
+print(nested_list_to_dict([[5, 2, 3, 4, 5], [4, 5, 6, 7, 8], [5, 6, 7, 8, 9]]))
+print(nested_list_to_dict([[3, 2, 3, 4, 5], [4, 5, 6, 7, 8], [5, 6, 7, 8]]))
