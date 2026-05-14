1c1f2af0de9c5adc86fcf3b0bafa77bc816ef018
never-settle-4-less <64497600+never-settle-4-less@users.noreply.github.com>
2026-05-14 18:59:08 +0530

    Add: python-max-heap - submission-0

---
+++ Python Coding Interviews/python-max-heap/submission-0.py
@@ -0,0 +1,17 @@
+import heapq
+from typing import List
+
+
+def get_reverse_sorted(nums: List[int]) -> List[int]:
+    heapq.heapify(nums)
+    nums.sort(reverse = True)
+    return nums
+
+
+
+
+
+# do not modify below this line
+print(get_reverse_sorted([1, 2, 3]))
+print(get_reverse_sorted([5, 6, 4, 2, 7, 3, 1]))
+print(get_reverse_sorted([5, 6, -4, 2, 4, 7, -3, -1]))
