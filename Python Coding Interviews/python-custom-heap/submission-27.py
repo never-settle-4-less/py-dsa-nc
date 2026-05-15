cdba25b73ae8666701825ef7bbee37722a06c1bd
never-settle-4-less <64497600+never-settle-4-less@users.noreply.github.com>
2026-05-15 17:21:39 +0530

    Add: python-custom-heap - submission-27

---
+++ Python Coding Interviews/python-custom-heap/submission-27.py
@@ -0,0 +1,27 @@
+import heapq
+from typing import List
+
+
+def get_reverse_sorted(nums: List[int]) -> List[int]:
+    
+    rev = []
+    heap = []
+
+    for num in nums:
+        pair = (-num, num)
+        heapq.heappush(heap, pair)
+    print(heap)
+        
+    
+    for i in heap:
+        j = heapq.heappop(heap)
+        k = j[1] 
+        rev.append(k)
+    return rev
+
+
+
+# do not modify below this line
+print(get_reverse_sorted([1, 2, 3]))
+print(get_reverse_sorted([5, 6, 4, 2, 7, 3, 1]))
+print(get_reverse_sorted([5, 6, -4, 2, 4, 7, -3, -1]))
